0e639c5f7c9dedbc1649bc2bdb3e21072f5f8778
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-22 17:43:51 +0100

    Created a function to choose difficulty

--- main.cpp
+++ main.cpp
@@ -17,11 +17,41 @@ std::string gameIntro()
     return name;
 }
 
+int setDifficulty()
+{
+    int choice = 0;
+
+    std::cout << "There are three difficulties to choose from, which would you like?" << std::endl;
+    std::cout << "1 - Easy\n2 - Normal\n3 - Impossible" << std::endl;
+
+    std::cin >> choice;
+
+    switch(choice)
+    {
+    case 1:
+        std::cout << "\nEasy difficulty has been selected" << std::endl;
+        break;
+    case 2:
+        std::cout << "\nNormal difficulty has been selected" << std::endl;
+        break;
+    case 3:
+        std::cout << "\nImpossible difficulty has been selected" << std::endl;
+        break;
+    default:
+        std::cout << "\nInvalid option, defaulting to Normal difficulty" << std::endl;
+        choice = 2;
+        break;
+    }
+
+    return choice;
+}
 
 int main()
 {
     std::string playerName = gameIntro();
+    int difficulty = setDifficulty();
 
+    std::cout << "Name: " << playerName << "\nDiff: " << difficulty << std::endl;
 
     return 0;
 }
