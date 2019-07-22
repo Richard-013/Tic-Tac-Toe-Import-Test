cbadfbe578868171b897c7983e9cbb3f87df2ece
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-22 17:43:29 +0100

    Created game intro to get player name

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+std::string gameIntro()
+{
+    std::string name = "";
+
+    std::cout << "Welcome to Tic-Tac-Toe, or Noughts and Crosses as some people may know it" << std::endl;
+    std::cout << "May I take your name before we get started?" << std::endl;
+
+    std::getline(std::cin, name);
+
+    std::cout << "\nWelcome " << name << std::endl;
+
+    return name;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::string playerName = gameIntro();
+
+
     return 0;
 }
