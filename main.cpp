d89fa0cce72d63c6300a9adbba0c47d81217167a
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-08-03 15:12:58 +0100

    Added  CheckWin function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 enum difficulty{Normal = 1, Impossible = 2};
 char grid[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
+bool checkWin(char symbol);
+int normalGame(std::string playerName);
+int impossibleGame(std::string playerName);
+
 std::string gameIntro()
 {
     std::string name = "";
@@ -64,9 +68,6 @@ void printGrid()
     }
 }
 
-int normalGame(std::string playerName);
-int impossibleGame(std::string playerName);
-
 int main()
 {
     clearScreen();
@@ -91,6 +92,57 @@ int main()
     }
 
     return 0;
+}
+
+bool checkWin(char symbol)
+{
+    // Checks outer lines
+    if(grid[0] == symbol)
+    {
+        if(grid[1] == symbol && grid[2] == symbol)
+        {
+            return true;
+        }
+        else if(grid[3] == symbol && grid[6] == symbol)
+        {
+            return true;
+        }
+    }
+    else if(grid[8] == symbol)
+    {
+        if(grid[6] == symbol && grid[7] == symbol)
+        {
+            return true;
+        }
+        else if(grid[2] == symbol && grid[5] == symbol)
+        {
+            return true;
+        }
+    }
+
+    // Checks lines that pass the central square of the grid
+    if(grid[4] == symbol)
+    {
+        if(grid[3] == symbol && grid[5] == symbol)
+        {
+            return true;
+        }
+        else if(grid[1] == symbol && grid[7] == symbol)
+        {
+            return true;
+        }
+        else if(grid[0] == symbol && grid[8] == symbol)
+        {
+            return true;
+        }
+        else if(grid[2] == symbol && grid[6] == symbol)
+        {
+            return true;
+        }
+    }
+
+    // No win condition found
+    return false;
 }
 
 int normalGame(std::string playerName)
