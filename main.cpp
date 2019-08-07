2accaa1906165f3509fcb45be64bae257df1b725
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-08-07 20:24:10 +0100

    Now prevents users from not entering a name

--- main.cpp
+++ main.cpp
@@ -367,13 +367,27 @@ void impossibleAITurn(char symbol)
 std::string gameIntro()
 {
     std::string name = "";
+    int marker = 0;
 
     std::cout << "Welcome to Tic-Tac-Toe, or Noughts and Crosses as some people may know it" << std::endl;
     std::cout << "May I take your name before we get started?\n" << std::endl;
 
-    std::getline(std::cin, name);
+    do
+    {
+        if(marker != 0)
+        {
+            std::cout << "\n\nI'm sorry, I didn't quite catch that, what was your name?\n" << std::endl;
+            std::getline(std::cin, name);
+        }
+        else
+        {
+            std::getline(std::cin, name);
+            marker++;
+        }
+    } while(name == "");
 
-    std::cout << "\nWelcome " << name << std::endl;
+    clearScreen();
+    std::cout << "\nWelcome " << name << "\n" << std::endl;
 
     return name;
 }
