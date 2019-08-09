9cf6ea19bce17804c1ae975a8220381a0bf629d6
Richard Horton <richardhorton97@outlook.com>
2019-08-09 13:06:41 +0100

    Implemented checkWin in Grid class

--- Grid.cpp
+++ Grid.cpp
@@ -4,3 +4,54 @@ Grid::Grid()
 {
     //ctor
 }
+
+bool Grid::checkWin(char symbol)
+{
+    // Checks outer lines
+    if(grid[0] == symbol)
+    {
+        if(grid[1] == symbol && grid[2] == symbol)
+        {
+            return true;
+        }
+        else if(grid[3] == symbol && grid[6] == symbol)
+        {
+            return true;
+        }
+    }
+    else if(grid[8] == symbol)
+    {
+        if(grid[6] == symbol && grid[7] == symbol)
+        {
+            return true;
+        }
+        else if(grid[2] == symbol && grid[5] == symbol)
+        {
+            return true;
+        }
+    }
+
+    // Checks lines that pass the central square of the grid
+    if(grid[4] == symbol)
+    {
+        if(grid[3] == symbol && grid[5] == symbol)
+        {
+            return true;
+        }
+        else if(grid[1] == symbol && grid[7] == symbol)
+        {
+            return true;
+        }
+        else if(grid[0] == symbol && grid[8] == symbol)
+        {
+            return true;
+        }
+        else if(grid[2] == symbol && grid[6] == symbol)
+        {
+            return true;
+        }
+    }
+
+    // No win condition found
+    return false;
+}
--- Grid.h
+++ Grid.h
@@ -6,12 +6,12 @@ class Grid
 {
     public:
         Grid();
+        bool checkWin(char symbol);
 
     protected:
 
     private:
         char grid[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}; // Grid the game is played on and that is displayed to the user
-        bool checkWin(char symbol);
         int checkChoice(int input);
         void printGrid();
         bool replaceSquare(char symbol, int input);
