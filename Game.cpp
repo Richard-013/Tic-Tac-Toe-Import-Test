d8741665ffbfb45ecd26956dd53de06dc1ef3e38
Richard Horton <richardhorton97@outlook.com>
2019-08-09 13:09:10 +0100

    Copied code for GameIntro into Game class
    Ready to be altered to work with the new structure of the game

--- Game.cpp
+++ Game.cpp
@@ -15,3 +15,30 @@ void Game::clearScreen()
     std::cout << std::string(100, '\n');
 }
 
+/*std::string Game::gameIntro()
+{
+    std::string name = "";
+    int marker = 0;
+
+    std::cout << "Welcome to Tic-Tac-Toe, or Noughts and Crosses as some people may know it" << std::endl;
+    std::cout << "May I take your name before we get started?\n" << std::endl;
+
+    do
+    {
+        if(marker != 0)
+        {
+            std::cout << "\n\nI'm sorry, I didn't quite catch that, what was your name?\n" << std::endl;
+            std::getline(std::cin, name);
+        }
+        else
+        {
+            std::getline(std::cin, name);
+            marker++;
+        }
+    } while(name == "");
+
+    clearScreen();
+    std::cout << "\nWelcome " << name << "\n" << std::endl;
+
+    return name;
+}*/
