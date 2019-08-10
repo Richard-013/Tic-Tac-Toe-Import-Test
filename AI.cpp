2e03dc142387e295541a4013fefffe37b2302253
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-08-10 20:13:27 +0100

    Barebones implementation of aiTurn to be finished later

--- AI.cpp
+++ AI.cpp
@@ -5,6 +5,28 @@ AI::AI()
     //ctor
 }
 
+void AI::aiTurn()
+{
+    if(difficulty == Impossible)
+    {
+        impossibleAITurn();
+    }
+    else
+    {
+        normalAITurn();
+    }
+}
+
+void AI::normalAITurn()
+{
+
+}
+
+void AI::impossibleAITurn()
+{
+
+}
+
 char AI::getSymbol()
 {
     // Returns the symbol the AI player is using
